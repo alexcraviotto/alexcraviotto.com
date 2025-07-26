a1ab91988244ba1ab511b8bfb7c35b94f6c6840a
alexcraviotto <alexcraviotto@pm.me>
2025-07-26 20:09:27 +0200

    feat: social redirections

--- next.config.mjs
+++ next.config.mjs
@@ -4,5 +4,34 @@ const nextConfig = {
         domains: ['i.scdn.co'],
         unoptimized: true,
     },
+    redirects: async () => {
+        return [
+            {
+                source: '/x',
+                destination: 'https://x.com/craviottoalex',
+                permanent: true,
+            },
+            {
+                source: '/twitter',
+                destination: 'https://x.com/craviottoalex',
+                permanent: true,
+            },
+            {
+                source: '/github',
+                destination: 'https://github.com/alexcraviotto',
+                permanent: true,
+            },
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/alexcraviotto',
+                permanent: true,
+            },
+            {
+                source: '/youtube',
+                destination: 'https://www.youtube.com/@craviottoalex',
+                permanent: true,
+            }
+        ]
+    },
 };
 export default nextConfig;
