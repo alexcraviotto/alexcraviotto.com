beb8ff570611a607041352484205503074fcadbd
alexcraviotto <alexcraviotto@pm.me>
2026-07-04 18:04:28 +0200

    feat: redirect all routes to home

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,9 @@
-export function middleware() {}
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/', request.url), 301)
+}
+
+export const config = {
+  matcher: ['/((?!_next|favicon.ico|images|api).+)'],
+}
